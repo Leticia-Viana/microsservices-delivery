d50e2d4f073a5737a7eff8cd4b45695ee6e9f935
Michelle <michelledantasluiz@gmail.com>
2023-04-10 20:14:58 -0300

    Creating microsservice-storage

---
+++ microsservice-storage/src/main/java/com/storage/microsservicestorage/MicrosserviceStorageApplication.java
@@ -0,0 +1,13 @@
+package com.storage.microsservicestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosserviceStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosserviceStorageApplication.class, args);
+	}
+
+}
